8560618e305f060885c70b193b1d0881238eff51
Ramesh Hariharan <rames1000@gmail.com>
2023-03-16 14:40:30 +0530

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,6 +1,10 @@
-# Create SparkSession from builder
-import pyspark
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.master("local[1]") \
-                    .appName('SparkByExamples.com') \
-                    .getOrCreate()
+from pyspark import SparkConf
+from pyspark import SparkContext
+
+conf = SparkConf()
+conf.setMaster('spark://LAPTOP-LAI03S4B.localdomain:7077')
+conf.setAppName('spark-basic')
+sc = SparkContext(conf=conf)
+
+txt = sc.textFile('text.csv')
+print(txt.count())
